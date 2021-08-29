667af9e48462e022b56f508ee0c29ce175bb4b41
Nikita Hiletin <nikitahiletin@gmail.com>
2021-08-30 02:09:16 +0300

    load data, classes update

--- akts_002.py
+++ akts_002.py
@@ -1,17 +1,23 @@
 import re as re
 import numpy as np
 import pandas as pd
-import argparse
+
+
+#import argparse
 
 app_version = 'AktsApp v 0.0.2 (22.08.2021)'
 
 def main():
     #define the logic of exe file
     app_help = InteractiveHelp()
+    A = SideData('SideA')
+    B = SideData('SideB')
 
     commands = {
         'help': app_help.gethelp,
-        'quit': quit
+        'quit': quit,
+        'load': load,
+        'dataheaders': dataheaders
     }
 
     while True:
@@ -39,8 +45,35 @@ def main():
 def cli_arg_parsing():
     cli_parser = argparse.ArgumentParser(name=app_version)
 
-class InteractiveHelp:
+def corey_logger(orig_foo):
+    import logging
+    logging.basicConfig(filename='{}.log'.format(orig_foo.__name__), level=logging.INFO)
+    
+    def wrapper(*args, **kwargs):
+        logging.info('Ran with args: {}, and kwargs: {}'.format(args, kwargs))
+        return orig_foo(*args, **kwargs)
+    
+    return wrapper    
 
+def load(sidename, filepath):
+    try:
+        SideData.get(sidename).upd_data(filepath)
+        print('Data successfully loaded to class instance')
+    except KeyError:    #заготовка на будущее
+        print('KeyError')
+
+def dataheaders(sidename):
+    try:
+        SideData.get(sidename).dataheaders()
+    except KeyError:    #заготовка на будущее
+        print('KeyError')
+
+class InteractiveHelp:
+    """
+    Объект для вызова интерактивной помощи
+    Переменные объекта - текстовые строки помощи
+    Словарь - для вызова по имени команды
+    """
     def __init__(self):
         self.default = 'Try using \'help\' or \'help command\' for more info'
         self.foo = 'This text describes the use of foo function'
@@ -54,12 +87,12 @@ class InteractiveHelp:
 
 
 class Command:
-    """Создает объект для работы с введенными командами
-    Принимает строку с командами.
+    """Создает объект для работы с введенной командой
+    Принимает строку с командами. 
     Атрибуты:
     user_command - основная команда (первое слово в строке)
     user_command_args - аргументы команды (кортеж)
-    user_command_args_n - количество аргументов
+    user_command_args_n - количество аргументов (хз зачем, пусть будет)
     """
     def __init__(self, inputstring):
         self.user_command = None
@@ -72,6 +105,41 @@ class Command:
                 self.user_command_args = tuple(self.inputlist[1:])
                 self.user_command_args_n = len(self.user_command_args)
 
+class SideData:
+    """Объект с данными сторон
+    попробуем закидывать данные в словарь
+    """
+
+    #словарь для хранения инстансов
+    sides_dict = {}
+
+    def __init__(self, name):
+        self.sidename = name
+        self.dataloaded = False
+        self.dataframesdict = dict()
+        SideData.sides_dict[name] = self #ПЕРЕДЕЛАТЬ ПРОВЕ
+
+    @corey_logger
+    def upd_data(self, filepath):
+        try:
+            self.dataframesdict.update(pd.read_excel(filepath, sheet_name=None))
+            self.dataloaded = True
+        except KeyError:    #заготовка на будущее
+            print('KeyError')
+
+    def dataheaders(self):
+        if self.dataloaded:
+            for sheet_name, dataframe in self.dataframesdict.items():
+                print(dataframe)
+        else:
+            print('No data loaded')
+
+    @classmethod
+    def get(cls, name):
+        return cls.sides_dict[name]
+
+    def __repr__(self):
+        return 'SideData instance, name: {}, dataloaded: {}'.format(self.sidename, self.dataloaded)
 
 if __name__ == '__main__':    
     main()
